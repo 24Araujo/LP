1221520ecb2a34dab0de1b4d6612881f181b9784
Hemily Araujo <hemilyaraujo277@gmail.com>
2023-08-23 17:54:56 -0300

    LIsta3

---
+++ Lista 3 - Operadores/ConversaoPrimitivos.java
@@ -0,0 +1,34 @@
+/*
+ * ConversaoPrimitivos.java
+ * 
+ * Copyright 2023 hemil <hemil@HEMILY>
+ * 
+ */
+
+public class  ConversaoPrimitivos {
+	
+	public static void main (String[] args) {
+		
+		int a = 10;
+		long b = (long) a;
+		int c = (int) b;
+		float d = (float) b;
+		short e = (short) a;
+		double f = (double) d;
+		Long g = b;
+		long h = g;
+		
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(d);
+		System.out.println(e);
+		System.out.println(f);
+		System.out.println(g);
+		System.out.println(h);
+		
+		
+		//Hemily De Araújo Ferraz
+	}
+}
+
---
+++ Lista 3 - Operadores/OperadoresDeAtribuicao.java
@@ -0,0 +1,60 @@
+/*
+ * OperadoresDeAtribuicao.java
+ * 
+ * Copyright 2023 hemil <hemil@HEMILY>
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ * 
+ * 
+ */
+
+
+public class OperadoresDeAtribuicao {
+	
+	public static void main (String[] args) {
+	
+	//O programa deve exibir os valores 1, 3, 2, 6, 3, 1, 2 e 1 nessa ordem.
+	
+		
+		int a = 1;
+		System.out.println(a); //1
+		
+		a += 2;
+		System.out.println(a); //3
+		
+		a -= 1;
+		System.out.println(a); //2
+		
+		a *= 3;
+		System.out.println(a); //6
+		
+		a /= 2;
+		System.out.println(a); //3
+		
+		a %= 2;
+		System.out.println(a); //1
+		
+		++a;
+		System.out.println(a); //2
+		
+		--a;
+		System.out.println(a); //1
+		
+		
+		//Hemily De Araújo Ferraz
+	}
+}
+
